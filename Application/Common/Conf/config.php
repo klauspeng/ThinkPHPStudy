7719e5be80b7d319f61cd971f6616ddfd1d6fd61
klaus <2511634167@qq.com>
2017-04-19 17:42:35 +0800

    模板文件

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,10 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+    //模块设置
+    'MODULE_ALLOW_LIST' => array('Home', 'Admin'),
+    'DEFAULT_MODULE'    => 'Home',
+    'CONTROLLER_LEVEL'  => 2,//多级控制器
+    'SHOW_PAGE_TRACE' =>true,//页面trace
+    'TMPL_CACHE_ON' => false,//禁止模板编译缓存
+    'HTML_CACHE_ON' => false,//禁止静态缓存
 );
